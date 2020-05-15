f4dce4e11ac27ad1e11a128d8638562f77e0450c
Matthew <mattcorralejo1@gmail.com>
2020-05-15 08:07:48 -0700

    passed associations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,4 @@
 class Doctor < ApplicationRecord
+    has_many :appointments
+    has many :patients, through: :appointments
 end
---
+++ db/migrate/20200515145127_add_foreign_key_to_appointments.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToAppointments < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :appointments, :doctor, foreign_key: true
+  end
+end
